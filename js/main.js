a2f45a78ca7d4441f593aa686e17c309dce790db
Navin Mohan <navinmohan81@gmail.com>
2017-05-10 21:48:25 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// Data
+var text = [
+  "Excel 2017",
+  "#RedefiningLines",
+  "Coming Soon.."
+];
+
+
+var scrollDelay = 8000; // delay for accepting the next scroll event
+var autoScrollDelay = 5000;
+// uncomment to override the defaults
+
+ var colors = ["#913cca","#bb3d7b", "#d63f4a","#da3f43", "#cc3e5c"];
+// var font = "Roboto"
+
+// particle-text init
+
+initScene(text[0]);
+requestAnimationFrame(render);
+
+
+// change on scroll
+
+
+var lock = true;
+var textOnDisplay = 0;
+
+setTimeout(() => { // prevent the user from scrolling too soon
+  lock = false;
+},scrollDelay);
+
+
+
+setInterval(() => {
+  console.log("set interval!");
+  while(lock){}
+  if (lock === false){
+    lock = true;
+    textOnDisplay = (textOnDisplay + 1) % text.length;
+    initScene(text[textOnDisplay]);
+    setTimeout(() => { // prevent the user from scrolling too soon
+      lock = false;
+    },scrollDelay);
+  }
+},scrollDelay + autoScrollDelay);
+
+
+Rx.Observable.fromEvent(window,'mousewheel')
+  .map( e => e.wheelDelta > 0)
+  .filter( () => lock === false )
+  .subscribe( (delta) => {
+    lock = true;
+    var t = delta?(textOnDisplay > 1 ? textOnDisplay - 1: 0 ): (textOnDisplay < text.length -1? textOnDisplay + 1:textOnDisplay)
+    console.log(delta,textOnDisplay);
+    if (t != textOnDisplay){
+      textOnDisplay = t;
+      initScene(text[textOnDisplay]);
+    }
+    setTimeout(() => {
+      lock = false;
+    },scrollDelay);
+  });
