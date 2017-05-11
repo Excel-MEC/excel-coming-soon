14883b48de7a9aa548efca5905efa9284a7d5647
Arjun Nair <arjunnair.mec@gmail.com>
2017-05-11 23:36:58 +0530

    Particles now change from last text position

--- js/particle-text.js
+++ js/particle-text.js
@@ -1,11 +1,14 @@
 var colors = ["#468966","#FFF0A5", "#FFB03B","#B64926", "#8E2800"];
 var font = "tahoma";
 var canvas = document.querySelector("#scene"),
-	ctx = canvas.getContext("2d"),
-	particles = [],
-	amount = 0,
-	mouse = {x:0,y:0},
-	radius = 0.7;
+ctx = canvas.getContext("2d"),
+particles = [],
+amount = 0,
+mouse = {x:0,y:0},
+radius = 0.7;
+
+var lastPosX = [];
+var lastPosY = [];
 
 
 var lasttext = ""; // for resize()
@@ -24,9 +27,9 @@ var radiusFactor = ww > 768?2500:3000;
 ww = ww > wh? ww: wh;
 wh = ww > wh? wh: ww;
 
-function Particle(x,y){
-	this.x =  Math.random()*ww;
-	this.y =  Math.random()*wh;
+function Particle(x, y, lastX, lastY){
+	this.x =  lastX;
+	this.y =  lastY;
 	this.dest = {
 		x : x,
 		y: y
@@ -76,28 +79,28 @@ function onMouseMove(e){
 }
 
 function onTouchMove(e){
-  if(e.touches.length > 0 ){
-    mouse.x = e.touches[0].clientX;
-    mouse.y = e.touches[0].clientY;
-  }
+	if(e.touches.length > 0 ){
+		mouse.x = e.touches[0].clientX;
+		mouse.y = e.touches[0].clientY;
+	}
 }
 
 function onTouchEnd(e){
-mouse.x = -9999;
-mouse.y = -9999;
+	mouse.x = -9999;
+	mouse.y = -9999;
 }
 
 function initScene(text){
 
   lasttext = text; // for resize
 
-	ww = canvas.width = window.innerWidth;
-	wh = canvas.height = window.innerHeight;
+  ww = canvas.width = window.innerWidth;
+  wh = canvas.height = window.innerHeight;
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	ctx.font = "bold "+Math.floor((ww/10)*(text.length >14 || fontSizeFactor === null ?14/text.length:fontSizeFactor))+"px " + font;
-	console.log(ctx.font);
+  ctx.font = "bold "+Math.floor((ww/10)*(text.length >14 || fontSizeFactor === null ?14/text.length:fontSizeFactor))+"px " + font;
+	// console.log(ctx.font);
 	ctx.textAlign = "center";
 	ctx.fillText(text, ww/2, wh/2);
 
@@ -105,16 +108,29 @@ function initScene(text){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.globalCompositeOperation = "screen";
 
+	console.log("length" + lastPosX.length);
 	particles = [];
+	var count = 0;
 	for(var i=0;i<ww;i+=Math.round(ww/densityFactor)){
 		for(var j=0;j<wh;j+=Math.round(ww/densityFactor)){
+
 			if(data[ ((i + j*ww)*4) + 3] > 150){
-				particles.push(new Particle(i,j));
+				count = count + 1;
+
+				if (lastPosX.length < count) {
+					lastPosX.push(Math.random()*ww);
+					lastPosY.push(Math.random()*wh);
+
+				}
+
+				particles.push(new Particle(i, j, lastPosX[count-1], lastPosY[count-1]));
 			}
 		}
 	}
 	amount = particles.length;
 
+	
+	console.log("no.of parts: " + amount)
 }
 
 function onMouseClick(){
@@ -129,6 +145,8 @@ function render(a) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (var i = 0; i < amount; i++) {
 		particles[i].render();
+		lastPosX[i] = particles[i].x;
+		lastPosY[i] = particles[i].y;
 	}
 };
 
